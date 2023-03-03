3a5b29dc82dc736fcdf4444b3ffd6d4491cf0a2a
zhaoshifeng <zhaoshifeng@tianta>
2023-03-04 00:57:59 +0800

    支付模块添加

---
+++ cloud-provider-payment8001/src/main/java/com/zhaoshf/cloud/domain/entity/Payment.java
@@ -0,0 +1,17 @@
+package com.zhaoshf.cloud.domain.entity;
+
+import lombok.Data;
+
+/**
+ * @Author：zhaoshf
+ * @Package：com.zhaoshf.cloud.domain.entity
+ * @name：Payment
+ * @Date：2023/3/4 0:29
+ */
+@Data
+public class Payment {
+
+    private Integer id;
+
+    private String serial;
+}
---
+++ cloud-provider-payment8001/src/main/java/com/zhaoshf/cloud/domain/service/PaymentService.java
@@ -0,0 +1,13 @@
+package com.zhaoshf.cloud.domain.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhaoshf.cloud.domain.entity.Payment;
+
+/**
+ * @Author：zhaoshf
+ * @Package：com.zhaoshf.cloud.domain.service
+ * @name：PaymentService
+ * @Date：2023/3/4 0:31
+ */
+public interface PaymentService extends IService<Payment> {
+}
